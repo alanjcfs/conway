a72768f1fe5a99091887b964390cd305b1536df2
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2016-04-13 17:24:27 -0700

    Add fmt::Display for Cell

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+use std::fmt;
 #[derive(Copy,Clone,Debug)]
 enum Cell {
-    O,
-    X
+    Dead,
+    Live
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Cell::Dead => write!(f, "{}", "0"),
+            &Cell::Live => write!(f, "{}", "1")
+        }
+    }
 }
 
 fn main() {
-    let mut table = [[Cell::O; 10]; 10];
+    let mut table = [[Cell::Dead; 10]; 10];
+    table[0][0] = Cell::Live;
+    table[0][1] = Cell::Live;
+
+    /* Go through each cell and check its adjacent cells to determine if it is
+     * alive or dead
+     */
+
+
     for row in table.into_iter() {
-        println!("{:?}", row);
+        for cell in row.into_iter() {
+            print!("{}", cell);
+        }
+        println!("");
     }
 }
