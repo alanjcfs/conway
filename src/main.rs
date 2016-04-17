a687a90926d5754e94334760f147bc29229bb109
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2016-04-17 13:24:19 -0700

    Switch to vectors

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,60 @@
 use std::fmt;
 #[derive(Copy,Clone,Debug)]
-enum Cell {
+enum State {
     Dead,
     Live
 }
 
-impl fmt::Display for Cell {
+#[derive(Copy,Clone,Debug)]
+struct Cell {
+    x: u32,
+    y: u32,
+    state: State
+}
+
+impl fmt::Display for State {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            &Cell::Dead => write!(f, "{}", "0"),
-            &Cell::Live => write!(f, "{}", "1")
+            &State::Dead => write!(f, "{}", "0"),
+            &State::Live => write!(f, "{}", "1")
         }
     }
 }
 
 fn main() {
-    let mut table = [[Cell::Dead; 10]; 10];
-    table[0][0] = Cell::Live;
-    table[0][1] = Cell::Live;
+    let mut table: Vec<Vec<Cell>> = vec![];
+    for x in 0..10 {
+        let mut row: Vec<Cell> = vec![];
+        for y in 0..10 {
+            row.push(Cell{x: x, y: y, state: State::Dead});
+        }
+        table.push(row);
+    }
 
     /* Go through each cell and check its adjacent cells to determine if it is
      * alive or dead
+     *   cccccccccc
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
+     * i 0000000000
      */
 
-
-    for row in table.into_iter() {
+    for row in table.clone().into_iter() {
         for cell in row.into_iter() {
-            print!("{}", cell);
+            print!("{}   ", cell.state);
+        }
+        println!("");
+    }
+    for row in table.clone().into_iter() {
+        for cell in row.into_iter() {
+            print!("{},{} ", cell.x, cell.y);
         }
         println!("");
     }
